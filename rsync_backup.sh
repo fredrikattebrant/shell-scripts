5d656298b121830b6a41507de44da034fef1a54a
Fredrik Attebrant <fxo@kfdd.mine.nu>
2015-06-20 20:19:53 +0200

    Fixed misc rewrite bugs including quoting to get rsync to use custom ssh port

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -24,9 +24,10 @@ echo $BACKUP_TIME > $BACKUPDIR_LOCAL/time.txt
 #rsync command
 rsync -avzhPR --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r --delete \
   --stats --log-file=$BACKUPDIR_LOCAL/rsync-${BACKUP_TIME}.log \
-  --exclude-from '~/exclude.txt' --link-dest=$BACKUPDIR_REMOTE/$(cat $BACKUPDIR_LOCAL/time2.txt) \
-  -e ssh -p $REMOTE_PORT $BACKUPDIR_LOCAL $REMOTE_USER@$REMOTE_HOST:$BACKUPDIR_REMOTE/${BACKUP_TIME}/
+  --link-dest=$BACKUPDIR_REMOTE/${BACKUP_TIME} \
+  -e "ssh -p $REMOTE_PORT" $BACKUPDIR_LOCAL $REMOTE_USER@$REMOTE_HOST:$BACKUPDIR_REMOTE/${BACKUP_TIME}/
 
+###  --exclude-from '~/exclude.txt' --link-dest=$BACKUPDIR_REMOTE/$(cat $BACKUPDIR_LOCAL/time2.txt) \
 #dont forget to scp the log file and put it with the backup
 scp -P $REMOTE_PORT $BACKUPDIR_LOCAL/rsync-${BACKUP_TIME}.log \
   $REMOTE_USER@$REMOTE_HOST:$BACKUPDIR_REMOTE/${BACKUP_TIME}/rsync-${BACKUP_TIME}.log
