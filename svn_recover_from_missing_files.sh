1f38dff542f8292f09dec5c7e7bde67ce9a31c09
Fredrik Attebrant <fredrik.attebrant@gmail.com>
2015-10-06 09:18:26 +0200

    Adds SVN recovery script

---
+++ svn_recover_from_missing_files.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/bash
+#
+# svn_recover_from_missing_files.sh
+# -dry[run] don't perform any operations, just show which files will be recovered to 'deleted' state
+#
+# Ref: http://fredrikattebrant.blogspot.co.uk/2015/01/xtext-and-svn-recovering-from-missing.html
+#
+# Recovers 'missing' files making them 'deleted'.
+#
+
+dryrun=false
+
+function usage {
+cat << EOH
+Usage: $(basename $0) [-dry[run]] path_to_svn_folder
+EOH
+}
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -dry*)
+      dryrun=true
+      shift
+      ;;
+    -h*|--h*|-?)
+      usage
+      exit 0
+      ;;
+    *)
+      break
+    esac
+done
+
+if [ -z "$1" ]
+then
+  # use current directory as svn folder to scan
+  svnfolder=.
+else
+  svnfolder="$1"
+fi
+
+svn status $svnfolder | grep '^! ' | awk '{print $2}' | sed 's-\\-/-g' > MISSING.txt
+filecount=$(cat MISSING.txt | wc -l)
+if [ $filecount -eq 0 ]
+then
+	echo "Found no missing files"
+	exit 0
+fi
+echo "Found: ${filecount} missing files"
+
+if [ "$dryrun" = "false" ]
+then
+  echo "Reverting $(cat MISSING.txt | xargs echo) ..."
+  cat MISSING.txt | xargs svn revert
+  echo "Deleting $(cat MISSING.txt | xargs echo) ..."
+  cat MISSING.txt | xargs svn delete
+  echo "Cleaning up ..."
+  rm MISSING.txt
+  echo "Done."
+  echo ""
+  echo "svn status:"
+  svn status $svnfolder
+else
+  echo "Only a dryrun - nothing changed."
+fi
