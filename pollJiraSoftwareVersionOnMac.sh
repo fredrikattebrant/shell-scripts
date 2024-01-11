f82142d19446a55fdf1b8a84878c00b2c4ddaf58
fredrikattebrant <fredrik.attebrant@gmail.com>
2024-01-11 09:43:08 +0100

    Add verbose flag (-v)

--- pollJiraSoftwareVersionOnMac.sh
+++ pollJiraSoftwareVersionOnMac.sh
@@ -9,8 +9,22 @@
 #
 #set -xv
 
-LAST_JIRA_VERSION_FILE=$HOME/.jira-sw-version-polled
+verbose=""
 
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -v*)
+      verbose="true"
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+LAST_JIRA_VERSION_FILE=$HOME/.jira-sw-version-polled
 PATH=/opt/homebrew/bin:/usr/local/bin:$PATH
 
 touch $LAST_JIRA_VERSION_FILE # create if missing
@@ -18,6 +32,11 @@ touch $LAST_JIRA_VERSION_FILE # create if missing
 CURRENT_JIRA_SOFTWARE_VERSION=$(cat $LAST_JIRA_VERSION_FILE)
 
 latest=$(python3 $HOME/bin/pollJiraSoftwareVersions.py | head -1)
+
+if [ ! -z "$verbose" ]
+then
+  echo "$latest"
+fi
 
 if [ "${CURRENT_JIRA_SOFTWARE_VERSION}" != "${latest}" ]
 then
